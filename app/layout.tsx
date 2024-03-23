976a6a1702c6a808e23cfc21d75cca550ee717fc
Cedrick John Supan <93630863+cedricksupan@users.noreply.github.com>
2024-03-23 22:50:26 +0800

    Add Next SEO package and update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,34 @@
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 import Providers from "./providers";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+
+const seo = {
+  title: "Cj Supan - Portfolio",
+  description: "cj supan's portfolio",
+  openGraph: {
+    type: "website",
+    url: "https://cjsupan-porfolio.vercel.app/",
+    title: "Cj Supan - Portfolio",
+    description: "cj supan's portfolio",
+    images: [
+      {
+        url: "https://imgur.com/a/ZyBrqgG",
+        width: 1200,
+        height: 630,
+        alt: "cj supan's portfolio",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@cjsupan",
+    site: "@cjsupan",
+    cardType: "summary_large_image",
+  },
+};
 
 const poppins = Poppins({
   subsets: ["latin"],
